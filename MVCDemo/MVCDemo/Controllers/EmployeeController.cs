936beebb983a168726d1df9828e1e744d43dcb1b
Sunkita <chandrasenreddy.sunkita@cgi.com>
2018-02-13 18:40:16 +0530

    Initial commit

---
+++ MVCDemo/MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmployeeListVM empList = new EmployeeListVM();
+            return View(empList.EmployeeList);
+        }
+
+        public ActionResult Details(int id)
+        {
+            EmployeeListVM empList = new EmployeeListVM();
+            var model = empList.EmployeeList.Where(item => item.Id == id).FirstOrDefault();
+            return View(model);
+        }
+    }
+}
---
+++ MVCDemo/MVCDemo/Models/EmployeeListVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class EmployeeListVM
+    {
+        public EmployeeListVM()
+        {
+
+            EmployeeList = new List<EmployeeVM>();
+            EmployeeList.Add(new EmployeeVM { Id = 1, Name = "Akash", Department = "Finance" });
+            EmployeeList.Add(new EmployeeVM { Id = 2, Name = "Chandrasen", Department = "Admin" });
+            EmployeeList.Add(new EmployeeVM { Id = 3, Name = "Rama", Department = "Finance" });
+        }
+
+        public List<EmployeeVM> EmployeeList { get; set; }
+    }
+}
